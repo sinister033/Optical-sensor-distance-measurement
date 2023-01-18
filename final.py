bd34322f757445fa1cf20aa0815572126e7c8fad
parapara29 <97225407+parapara29@users.noreply.github.com>
2023-01-19 01:05:49 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,3 +1,4 @@
+from os import confstr
 from dictionaries import units, xyModes
 import mouse
 import struct
@@ -5,8 +6,8 @@ import math
 import time
 from random import randint
 
-f = open( "/dev/input/mice", "rb" ); 
 
+f = open( "/dev/input/mice", "rb" ); 
 
 
 quadrentCodes = [8,24,56,40]
@@ -30,10 +31,13 @@ distanceDots = 0
 
 currentUnitIndex = 0
 currentModeIndex = 0
+bluetoothConnected = False
+blue_com = None
 
 unitKeys = list(units.keys())
 unit = unitKeys[currentUnitIndex]
 mode = xyModes[currentModeIndex]
+
 
 def cycleUnits():
   global unit, currentUnitIndex
@@ -57,6 +61,8 @@ def cycleModes():
 
 
 
+
+
 def incrementDots(increment): 
   global distanceDots 
   distanceDots = distanceDots + increment
@@ -66,12 +72,12 @@ def resetDots():
   distanceDots = 0
 
 def print_output(distanceDots, divisor):
-  # global distance_travelled
   print(distanceDots/divisor, unit)
-  # distance_travelled = distanceDots/divisor
 
-def get_data():
-  global var
+      
+
+def main():
+  
   while 1:
     data = f.read(3)  # Reads the 3 bytes 
     
@@ -79,27 +85,25 @@ def get_data():
     
     print(tuple)
     if tuple[0] in leftClickCodes:
+
       if currentModeIndex == 0:
-          incrementDots( tuple[1] )
-          break
-      elif currentModeIndex == 1:
-          incrementDots(abs(tuple[1]) )
-      elif currentModeIndex == 2:
-          incrementDots( tuple[2] )
-      elif currentModeIndex == 3:
-          incrementDots(abs(tuple[2]) )
-      elif currentModeIndex == 4:
           incrementDots(math.sqrt( tuple[1]**2 + tuple[2]**2))
+      elif currentModeIndex == 1:
+          incrementDots( tuple[1] )
+      elif currentModeIndex == 2:
+          incrementDots( math.abs(tuple[1]) )
+      elif currentModeIndex == 3:
+          incrementDots( tuple[2] )
+      elif currentModeIndex == 4:
+          incrementDots( math.abs(tuple[2]) )
       else:
           incrementDots(math.sqrt( tuple[1]**2 + tuple[2]**2))
-      # if mouse.is_pressed(button='left') == False:
-      #   print_output(distanceDots, units[unit])
-      var = distanceDots/units[unit]
-      print(var)
+
+      print_output(distanceDots, units[unit])
       mouse.move("500", "500")
     
     elif tuple[0] in rightClickCodes:
-      resetDots()
+      cycleUnits()
       print_output(distanceDots, units[unit])
       mouse.move("500", "500")
 
@@ -107,10 +111,12 @@ def get_data():
     elif tuple[0] in midClickCodes:
       cycleModes()
       print_output(distanceDots, units[unit])
-      mouse.move("500", "500")     
+      mouse.move("500", "500")
+    
+    elif tuple[0] in midLeftCodes:
+      resetDots()
+      print_output(distanceDots, units[unit])
+      mouse.move("500", "500")
 
-def comm():
-  get_data()
-  return distanceDots, units[unit], unit, var
 
-
+if __name__ == "__main__": main()
